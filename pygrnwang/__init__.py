67d913535c6c113838bdafb6d6d6804ac168c870
Zhou-Jiangcheng <zhoujcpku@pku.edu.cn>
2026-06-27 20:57:01 +0800

    fix typo

--- pygrnwang/__init__.py
+++ pygrnwang/__init__.py
@@ -3,8 +3,8 @@ pygrnwang
 ==========
 This Python package serves as the frontend for calculating and building a Green's function library
 for synthetic seismograms. The backend consists of Wang Rongjiang's program for calculating synthetic
-seismograms in layered Earth models, including EDGRN/EDCMP QSEIS, SPGRN, and QSSP (Wang, 1999;
-Wang et al., 2017).
+seismograms in layered Earth models, including EDGRN/EDCMP, QSEIS, SPGRN, and QSSP (Wang, 1999;
+Wang, 2003; Wang and wang, 2007; Wang et al., 2017; Zhou et al., 2026).
 
 """
 
